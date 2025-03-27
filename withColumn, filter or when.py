4b66a3db015a3b559ca9afe3bb9502c8df64205c
Jean Jimbo <119676372+jean-jimbo-sonarsource@users.noreply.github.com>
2025-03-27 10:32:31 +0100

    misuse of withColumn, filter or when

---
+++ withColumn, filter or when.py
@@ -0,0 +1,4 @@
+from pyspark.sql.functions import *
+df = df.withColumn('Revenue', col('fare_amount').substr(0, 10).cast("float") + col('extra').substr(0, 5).cast("float") + col('tax').substr(0, 3).cast("float")) # Noncompliant
+df = df.withColumn('High revenue', when(col('fare_amount').substr(0, 10).cast("float") > 100. and col('extra').substr(0, 5).cast("float") > 100. and col('tax').substr(0, 3).cast("float") < 50.)) # Noncompliant
+df = df.filter(col('fare_amount').substr(0, 10).cast("float") > 100. and col('extra').substr(0, 5).cast("float") > 100. and col('tax').substr(0, 3).cast("float") < 50.) # Noncompliant
